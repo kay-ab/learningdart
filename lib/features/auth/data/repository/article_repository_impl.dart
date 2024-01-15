00c5924e38d28c3c87366beb848078c8ae54b1e4
kay <kay@aluto-benli.com>
2024-01-15 15:20:12 +0630

    Fix api article response

--- lib/features/auth/data/repository/article_repository_impl.dart
+++ lib/features/auth/data/repository/article_repository_impl.dart
@@ -1,13 +1,37 @@
 import 'package:learningdart/core/resources/data_state.dart';
+// import 'package:learningdart/features/auth/data/data_sources/remote/news_api_service.dart';
+import '../data_sources/remote/news_api_service.dart';
 import 'package:learningdart/features/auth/data/models/article.dart';
 import 'package:learningdart/features/auth/domain/entities/article.dart';
 import 'package:learningdart/features/auth/domain/repository/article_repository.dart';
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:learningdart/core/constants/constants.dart';
 
 class ArticleRepositoryImpl implements ArticleRepository {
-    @override
-    Future<DataState<List<ArticleModel>>> getNewsArticles() {
-        // TODO: implement getNewsArticles
-        throw UnimplementedError();
+    final NewsApiService _newsApiService;
+    ArticleRepositoryImpl(this._newsApiService);
+    Future<DataState<List<ArticleModel>>> getNewsArticles() async {
+    try {
+        final httpResponse = await _newsApiService.getNewsArticles(
+            apiKey: newsAPIKey,
+            country: countryQuery,
+            category: categoryQuery,
+        );
+
+        if (httpResponse.response.statusCode == HttpStatus.ok) {
+            return DataSuccess(httpResponse.data);
+        } else {
+            return DataFailed(DioException(
+                error: httpResponse.response.statusMessage,
+                response: httpResponse.response,
+                type: DioExceptionType.badResponse,
+                requestOptions: httpResponse.response.requestOptions));
+        }
+        } on DioException catch (e) {
+            return DataFailed(e);
+        }
     }
 
     @override
